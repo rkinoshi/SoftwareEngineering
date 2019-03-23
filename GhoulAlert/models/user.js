00586eda9e2e9be8d9198ec05981873da6a4e30a
Michael Nizzari <michael.nizzari@gmail.com>
2019-03-23 11:02:01 -0400

    users model altered for use with passport

--- GhoulAlert/models/user.js
+++ GhoulAlert/models/user.js
@@ -1,14 +1,47 @@
 'use strict';
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 module.exports = (sequelize, DataTypes) => {
   // Define the user model
   var User = sequelize.define('User', {
     // The user has a "username", of the string datatype
     username: DataTypes.STRING,
-    // The user has a "password", also of the string datatype
-    // TEMPORARY - A more secure option will be used when authorization is integrated
-    password: DataTypes.STRING
+
+    // The user has a "password", composed of a hash and salt
+    hash: DataTypes.STRING,
+    salt: DataTypes.STRING
   })
+
+  User.prototype.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  }
+
+  User.prototype.validatePassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+  }
+
+  User.prototype.generateJWT = function() {
+    const today = new Date();
+    const expires = new Date(today);
+    expires.setDate(today.getDate() + 30);
+
+    return jwt.sign({
+      username: this.username,
+      id: this._id,
+      exp: parseInt(expires.getTime() / 1000, 10),
+    }, 'secret');
+  }
+
+  User.prototype.toAuthJSON = function() {
+      return {
+        _id: this._id,
+        username: this.username,
+        token: this.generateJWT()
+      };
+  }
 
   return User;
 }
